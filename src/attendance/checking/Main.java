7f2af4a34c1407968be7b4df011ff50b0759796f
Abhinav Raj <iamabhinav118@gmail.com>
2021-06-17 04:59:59 +0530

    Initial Commit;

---
+++ src/attendance/checking/Main.java
@@ -0,0 +1,14 @@
+package attendance.checking;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        CheckFile file = new CheckFile();
+        try {
+            file.checkAttendanceFile();
+        } catch (IOException ex) {
+            System.out.println("File not Found");
+        }
+    }
+}
+
+
